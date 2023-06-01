330d161900fdffe6e0cc69e857bbbf1114716851
Musadaq hanif <musadaqhanif678@gmail.com>
2023-06-01 20:28:51 +0500

    Add files via upload

---
+++ store.js
@@ -0,0 +1,14 @@
+let mainimage = document.getElementById("mainimagez");
+let ali = document.getElementsByClassName("small");
+ali[0].onclick = function () {
+    mainimage.src = ali[0].src;
+}
+ali[1].onclick = function () {
+    mainimage.src = ali[1].src;
+}
+ali[2].onclick = function () {
+    mainimage.src = ali[2].src;
+}
+ali[3].onclick = function () {
+    mainimage.src = ali[3].src;
+}
